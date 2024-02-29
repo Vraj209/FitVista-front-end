d39c1ce2b86c01d5eb0b8a8a116ce23a9725aa45
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2024-02-28 19:00:30 -0500

    Add axios and react-hot-toast dependencies, implement user signup functionality

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,22 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import "./Signup.css";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 function Signup() {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    role: "",
+    email: "",
+    password: "",
+  });
+  const [disable, setDisable] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const signup = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/users/signup",
+        user
+      );
+      console.log("Response from signup", response.data);
+      setLoading(false);
+      navigate("/signin");
+    } catch (error) {
+      console.log("Error in signup", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.firstName.length > 0 &&
+      user.lastName.length > 0 &&
+      user.role.length > 0 &&
+      user.email.length > 0 &&
+      user.password.length > 0
+    ) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+  }, [user]);
+
   return (
     <div className="registerContainer">
       <div className="registerSide">
-        <form className="registerForm">
-          <h2 className="text-3xl font-semibold">Sign Up</h2>
+        <form className="registerForm" onSubmit={signup}>
+          <h2 className="text-3xl font-semibold">
+            {loading ? "Procressing" : "Sign up"}
+          </h2>
           <div className="inputField">
-            <input type="text" placeholder="First Name" required />
+            <input
+              type="text"
+              placeholder="First Name"
+              value={user.firstName}
+              onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+              required
+            />
           </div>
           <div className="inputField">
-            <input type="text" placeholder="Last Name" required />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={user.lastName}
+              onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+              required
+            />
           </div>
           <div className="inputField">
             <select
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black-500 focus:border-black-500 block w-full p-2.5"
+              value={user.role}
+              onChange={(e) => setUser({ ...user, role: e.target.value })}
               required
             >
               <option value="" disabled selected>
@@ -28,17 +88,29 @@ function Signup() {
             </select>
           </div>
           <div className="inputField">
-            <input type="email" placeholder="Email" required />
+            <input
+              type="email"
+              placeholder="Email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              required
+            />
           </div>
 
           <div className="inputField">
-            <input type="password" placeholder="Password" required />
+            <input
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              required
+            />
           </div>
           <button
             className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
             type="submit"
           >
-            Register
+            {disable ? "Please Fill your details" : "Sign Up"}
           </button>
           <p className="signinText">
             Already registered? <Link to="/signin">Login Here!</Link>
