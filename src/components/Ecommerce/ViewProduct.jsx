1cefc62ab6b537134e9bd51e66498352024444c6
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2024-03-05 09:10:20 -0500

    Refactor ViewProduct component layout and styles

--- src/components/Ecommerce/ViewProduct.jsx
+++ src/components/Ecommerce/ViewProduct.jsx
@@ -23,43 +23,45 @@ function ViewProduct() {
   }, [id]);
 
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex">
       <Sidebar />
-      <div className="flex-grow justify-center">
-        <div className="flex-grow p-5 bg-gray-100 min-h-screen">
-          <div className="max-w-4xl mx-auto py-10">
-            <div className="bg-white shadow-xl rounded-lg overflow-hidden">
-              {/* Increase the height of the image container */}
-              <div
-                className="bg-cover bg-center h-96 p-4"
-                style={{ backgroundImage: `url(${product.image})` }}
-              >
-                <div className="flex justify-end">
-                  <span className="text-lg text-white bg-indigo-600 hover:bg-indigo-500 font-bold py-2 px-4 rounded-full">
-                    ${product.price}
-                  </span>
-                </div>
-              </div>
-              <div className="p-4">
-                <h3 className="text-2xl text-gray-900 font-bold">
-                  {product.name}
-                </h3>
-                <p className="text-gray-600 mt-2">{product.shortDescription}</p>
-              </div>
-              <div className="p-4 border-t border-gray-200">
-                <span className="font-bold text-gray-900">Category:</span>{" "}
-                {product.category}
+      <div className="flex-1 p-5 bg-gray-100 min-h-screen">
+        <div className="max-w-5xl mx-auto py-10">
+          <div className="bg-white shadow-xl rounded-lg overflow-hidden flex md:flex-row flex-col">
+            {/* Left Side: Image Container */}
+            <div
+              className="md:w-1/2 h-auto bg-cover bg-center"
+              style={{ backgroundImage: `url(${product.image})` }}
+            ></div>
+
+            {/* Right Side: Product Details */}
+            <div className="md:w-1/2 p-4 flex flex-col justify-between">
+              {/* Price Tag at the Top */}
+              <div className="flex justify-between items-center">
+                <span className="text-2xl md:text-3xl lg:text-4xl text-gray-900 font-bold bg-green-500 text-white py-1 px-3 rounded-full shadow-lg">
+                  ${product.price}
+                </span>
               </div>
 
-              <div className="p-4 border-t border-b border-gray-200">
-                <span className="font-bold text-gray-900">Description:</span>{" "}
-                {product.description}
+              <div>
+                <h3 className="text-3xl text-gray-900 font-bold mt-4">
+                  {product.name}
+                </h3>
+                <p className="text-gray-700 mt-2">{product.shortDescription}</p>
+                <div className="mt-4">
+                  <span className="font-bold text-gray-900">Category:</span>{" "}
+                  {product.category}
+                </div>
+                <div className="mt-4 mb-4">
+                  <span className="font-bold text-gray-900">Description:</span>{" "}
+                  {product.description}
+                </div>
               </div>
-              <div className="flex justify-between items-center p-4 border-t border-gray-200">
-                <button className="w-1/4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">
+              <div className="flex flex-col sm:flex-row justify-between items-center mt-4 gap-5">
+                <button className="mb-2 sm:mb-0 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full transition ease-in-out duration-150">
                   <Link to="/ecommerce">Go Back</Link>
                 </button>
-                <button className="w-1/4 bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded transition ease-in-out duration-150">
+                <button className="text-sm bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded-full transition ease-in-out duration-150">
                   Add to Cart
                 </button>
               </div>
