a352f842945b294293cbd0c41f8d033bf89d032e
Vraj <80612219+Vraj209@users.noreply.github.com>
2025-06-11 23:26:30 -0400

    change in homepage

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   const logout = () => {
     setAuth({ userData: null, accessToken: null });
   };
-  return accessToken ? (
+  return (
     <div>
       {/* hero section */}
       <div
@@ -267,23 +267,6 @@ function Home() {
           </div>
         </div>
       </footer>
-    </div>
-  ) : (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-        <div className="mb-4">
-          <p className="text-gray-700 text-lg">Oops! You are not logged in.</p>
-          <p className="text-gray-600 text-base">
-            Please log in to see the content.
-          </p>
-        </div>
-        <Link
-          to="/signin"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Login
-        </Link>
-      </div>
     </div>
   );
 }
